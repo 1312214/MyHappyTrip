b92ecfda9747beeaa654186b46f9ca20068e7d8e
1312214 <nguyenduyhoangsmith@gmail.com>
2017-06-02 10:00:15 +0700

    modify layout/activity_place.xml

--- app/src/main/java/com/project/rogerh/happytrip/PlaceActivity.java
+++ app/src/main/java/com/project/rogerh/happytrip/PlaceActivity.java
@@ -3,9 +3,10 @@ package com.project.rogerh.happytrip;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -13,7 +14,8 @@ import org.w3c.dom.Text;
 
 public class PlaceActivity extends AppCompatActivity {
     TextView txtTenDiaDiem, txtGioiThieu, txtComment, txtDiaChi;
-    ImageView imgDiaDiem;
+    ImageView imgDiaDiem, btnAddList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +39,20 @@ public class PlaceActivity extends AppCompatActivity {
         txtComment = (TextView) findViewById(R.id.comment_user);
         txtDiaChi = (TextView) findViewById(R.id.DiaChi_ChiTiet);
         imgDiaDiem = (ImageView) findViewById(R.id.CacHinhAnhDiaDiem);
+        btnAddList = (ImageView) findViewById(R.id.Button_AddList);
 
         txtComment.setText(myObject.comments.get(1));
         txtGioiThieu.setText(myObject.intro);
         txtDiaChi.setText(myObject.address);
         imgDiaDiem.setImageResource(myObject.images.get(1));
+
+        btnAddList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Đã thêm địa điểm này vào danh sách Quan tâm", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
 
     }
